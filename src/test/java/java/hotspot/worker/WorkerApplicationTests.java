16c0dc603a5c52e7490564f51280410301094ad4
andrew <leegd120@gmail.com>
2026-02-11 00:27:03 +0900

    init: create usage event worker project

---
+++ src/test/java/java/hotspot/worker/WorkerApplicationTests.java
@@ -0,0 +1,13 @@
+package java.hotspot.worker;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class WorkerApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
