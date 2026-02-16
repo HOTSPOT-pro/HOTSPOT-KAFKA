750976de7aebd387e0cbaa539406dfb5555e1d61
Jae <lijae3552@gmail.com>
2026-02-17 04:09:20 +0900

    [HOTSPOT-65] feat: Redis 보조 설정 추가

---
+++ src/main/java/hotspot/worker/consumer/config/RedisClientConfig.java
@@ -0,0 +1,21 @@
+package hotspot.worker.consumer.config;
+
+import java.time.ZoneId;
+
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+
+import hotspot.worker.consumer.support.RedisKeyBuilder;
+
+/**
+ * Consumer에서 사용하는 Redis 보조 설정
+ */
+@Configuration
+public class RedisClientConfig {
+
+    // Redis 키 규칙 빌더를 KST 기준으로 생성
+    @Bean
+    public RedisKeyBuilder redisKeyBuilder() {
+        return new RedisKeyBuilder(ZoneId.of("Asia/Seoul"));
+    }
+}
