1174359d8650af58ab92a967ddd5bf0fd29990a3
andrew <leegd120@gmail.com>
2026-02-17 14:04:46 +0900

    [HOTSPOT-63] feat : Spotless Check 실행

--- src/main/java/hotspot/worker/producer/scheduler/UsageScheduler.java
+++ src/main/java/hotspot/worker/producer/scheduler/UsageScheduler.java
@@ -1,10 +1,6 @@
 package hotspot.worker.producer.scheduler;
 
-import hotspot.worker.producer.generator.UsageGenerator;
-import lombok.RequiredArgsConstructor;
-import lombok.extern.slf4j.Slf4j;
-import org.springframework.scheduling.annotation.Scheduled;
-import org.springframework.stereotype.Component;
+
 
 //@Slf4j
 //@Component
