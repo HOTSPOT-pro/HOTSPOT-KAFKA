bedf17f418edfb8dd79c86669c6746e94e08f205
Andrew <98813748+starboxxxx@users.noreply.github.com>
2026-02-20 17:08:10 +0900

    Update WorkerApplication.java

--- src/main/java/hotspot/worker/WorkerApplication.java
+++ src/main/java/hotspot/worker/WorkerApplication.java
@@ -4,12 +4,16 @@ import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.scheduling.annotation.EnableScheduling;
 
-@EnableScheduling
+//@EnableScheduling
 @SpringBootApplication
 public class WorkerApplication {
 
     public static void main(String[] args) {
+    if (Arrays.asList(args).contains("seed")) {
+        SpringApplication.run(SeedRunner.class, args);
+    } else {
         SpringApplication.run(WorkerApplication.class, args);
     }
+}
 
 }
