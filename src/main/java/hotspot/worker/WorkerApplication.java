f8d558355800ab5473484a3576c1c9056205c66c
Andrew <98813748+starboxxxx@users.noreply.github.com>
2026-02-20 18:01:25 +0900

    Enable scheduling in WorkerApplication

--- src/main/java/hotspot/worker/WorkerApplication.java
+++ src/main/java/hotspot/worker/WorkerApplication.java
@@ -7,7 +7,7 @@ import org.springframework.boot.autoconfigure.SpringBootApplication;
 
 import hotspot.seed.SeedRunner;
 
-//@EnableScheduling
+@EnableScheduling
 @SpringBootApplication
 public class WorkerApplication {
 
