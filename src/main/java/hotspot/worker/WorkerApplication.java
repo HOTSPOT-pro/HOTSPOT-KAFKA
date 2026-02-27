bdc41060d0fd5cdc77d47f0b24044ced9a85457a
Andrew <98813748+starboxxxx@users.noreply.github.com>
2026-02-27 15:25:11 +0900

    Comment out @EnableScheduling in WorkerApplication
    Comment out the @EnableScheduling annotation.

--- src/main/java/hotspot/worker/WorkerApplication.java
+++ src/main/java/hotspot/worker/WorkerApplication.java
@@ -8,7 +8,7 @@ import org.springframework.scheduling.annotation.EnableScheduling;
 
 import hotspot.seed.SeedRunner;
 
-@EnableScheduling
+//@EnableScheduling
 @SpringBootApplication
 public class WorkerApplication {
 
