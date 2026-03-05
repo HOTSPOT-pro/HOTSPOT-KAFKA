86736c0a9d3af3549b6c139350c11dfb850755fa
andrew <leegd120@gmail.com>
2026-03-06 03:20:55 +0900

    [HOTSPOT-265] fix : spotless 에러 해결

--- src/main/java/hotspot/worker/consumer/consistency/family/handler/FamilyPolicyDeactivateHandler.java
+++ src/main/java/hotspot/worker/consumer/consistency/family/handler/FamilyPolicyDeactivateHandler.java
@@ -1,9 +1,11 @@
 package hotspot.worker.consumer.consistency.family.handler;
 
+import org.springframework.stereotype.Component;
+
 import com.fasterxml.jackson.databind.JsonNode;
+
 import hotspot.worker.consumer.consistency.family.executor.FamilyLuaExecutor;
 import lombok.RequiredArgsConstructor;
-import org.springframework.stereotype.Component;
 
 @Component
 @RequiredArgsConstructor
