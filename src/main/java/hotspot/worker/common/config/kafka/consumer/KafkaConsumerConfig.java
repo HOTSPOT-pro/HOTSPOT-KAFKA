b77e479ac4a5cc144c3a9e40963e86a232dc687a
Jae <lijae3552@gmail.com>
2026-03-17 20:51:53 +0900

    [HOTSPOT-361] fix: Consumer 내부 병렬 스레드 개수 변경

--- src/main/java/hotspot/worker/common/config/kafka/consumer/KafkaConsumerConfig.java
+++ src/main/java/hotspot/worker/common/config/kafka/consumer/KafkaConsumerConfig.java
@@ -38,7 +38,7 @@ public class KafkaConsumerConfig {
                 new ConcurrentKafkaListenerContainerFactory<>();
 
         factory.setConsumerFactory(usageConsumerFactory);
-        factory.setConcurrency(6);
+        factory.setConcurrency(20);
         factory.getContainerProperties()
                 .setAckMode(ContainerProperties.AckMode.MANUAL);
         factory.getContainerProperties().setAsyncAcks(true);
