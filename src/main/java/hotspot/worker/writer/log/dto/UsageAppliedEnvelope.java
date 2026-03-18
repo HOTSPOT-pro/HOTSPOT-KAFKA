2750b0b2b3fa1291215234948c3c764dc599c738
Jae <lijae3552@gmail.com>
2026-03-18 14:14:39 +0900

    [HOTSPOT-365] refactor: 개별 Acknowledgment를 UsageBatchAcknowledgment로 교체

--- src/main/java/hotspot/worker/writer/log/dto/UsageAppliedEnvelope.java
+++ src/main/java/hotspot/worker/writer/log/dto/UsageAppliedEnvelope.java
@@ -2,18 +2,17 @@ package hotspot.worker.writer.log.dto;
 
 import java.util.List;
 
-import org.springframework.kafka.support.Acknowledgment;
-
 import hotspot.worker.consumer.usage.domain.UsageLuaResult;
 import hotspot.worker.consumer.usage.schema.UsageEvent;
 import hotspot.worker.outbox.infrastructure.entity.NotificationOutboxEventEntity;
 import hotspot.worker.writer.infrastructure.entity.UsageAppliedEventLogEntity;
+import hotspot.worker.writer.log.support.UsageBatchAcknowledgment;
 
 public record UsageAppliedEnvelope(
         UsageEvent event,
         UsageLuaResult result,
         List<NotificationOutboxEventEntity> outboxEvents,
         UsageAppliedEventLogEntity usageAppliedLog,
-        Acknowledgment acknowledgment
+        UsageBatchAcknowledgment batchAcknowledgment
 ) {
 }
