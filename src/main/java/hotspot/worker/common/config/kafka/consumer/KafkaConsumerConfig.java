d5c8c370054cf427ed7fa1016487d580368cbf61
Jae <lijae3552@gmail.com>
2026-03-19 20:07:45 +0900

    fix: concurrency 수정

--- src/main/java/hotspot/worker/common/config/kafka/consumer/KafkaConsumerConfig.java
+++ src/main/java/hotspot/worker/common/config/kafka/consumer/KafkaConsumerConfig.java
@@ -38,7 +38,7 @@ public class KafkaConsumerConfig {
                 new ConcurrentKafkaListenerContainerFactory<>();
 
         factory.setConsumerFactory(usageConsumerFactory);
-        factory.setConcurrency(18);
+        factory.setConcurrency(24);
         factory.setBatchListener(true);
         factory.getContainerProperties()
                 .setAckMode(ContainerProperties.AckMode.MANUAL);
