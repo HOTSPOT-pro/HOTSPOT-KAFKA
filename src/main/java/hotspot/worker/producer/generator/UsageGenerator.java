ae6729c3ead18dd6d3beed14eb79cab09f31f692
Jae <lijae3552@gmail.com>
2026-03-19 10:49:02 +0900

    fix: TPS 7000으로 변경

--- src/main/java/hotspot/worker/producer/generator/UsageGenerator.java
+++ src/main/java/hotspot/worker/producer/generator/UsageGenerator.java
@@ -34,7 +34,7 @@ public class UsageGenerator {
 
     private static final int MIN_USAGE_KB = 50;
     private static final int MAX_USAGE_KB = 500;
-    private static final int EVENT_SIZE = 10000;
+    private static final int EVENT_SIZE = 7000;
 
     public void produceEvent() {
         List<UsageEvent> events = generateRandomEvents();
