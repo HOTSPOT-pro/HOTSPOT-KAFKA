fcf71e5ed744d05ff5cbe9f545f5df4126b82079
Andrew <98813748+starboxxxx@users.noreply.github.com>
2026-03-19 15:28:14 +0900

    fix: Concurrency 변경

--- src/main/java/hotspot/worker/common/config/kafka/consumer/KafkaConsumerConfig.java
+++ src/main/java/hotspot/worker/common/config/kafka/consumer/KafkaConsumerConfig.java
@@ -38,7 +38,7 @@ public class KafkaConsumerConfig {
                 new ConcurrentKafkaListenerContainerFactory<>();
 
         factory.setConsumerFactory(usageConsumerFactory);
-        factory.setConcurrency(12);
+        factory.setConcurrency(18);
         factory.setBatchListener(true);
         factory.getContainerProperties()
                 .setAckMode(ContainerProperties.AckMode.MANUAL);
